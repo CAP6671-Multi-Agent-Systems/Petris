d337ee426efb763c00cfb31a89db579ddb76c97b
stefanwerleman <stefanwerleman@knights.ucf.edu>
2023-04-12 21:19:39 -0400

    Create policy saver factory function

---
+++ src/policy_saver/policy_saver.py
@@ -0,0 +1,29 @@
+"""Policy Saver module"""
+
+import logging
+
+from tf_agents.agents.tf_agent import TFAgent
+from tf_agents.policies.policy_saver import PolicySaver
+
+from src import paths
+
+logger = logging.getLogger(__name__)
+
+
+POLICY_DIR = paths.BASE_DIR / "policy"
+
+
+def create_policy_saver(agent: TFAgent) -> PolicySaver:
+    """
+    Creates a new policy saver for the 'agent' as well as the 'policy' directory.
+
+    Args:
+        agent (TFAgent): Agent being trained
+
+    Returns:
+        PolicySaver: PolicySaver instance to use
+    """
+    
+    POLICY_DIR.mkdir(exist_ok=True)
+    
+    return PolicySaver(policy=agent.policy)
